09bb2d3182e0df4dda830aefeed69a98c089b8d3
REZAN <shahriarasif753@gmail.com>
2022-09-30 18:19:09 +0600

    fixed bug

--- app/src/main/java/com/example/madproject/MainActivity.java
+++ app/src/main/java/com/example/madproject/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-        replaceFregment(new SetttingsFragment());
+        replaceFregment(new ListFragment());
 
         setContentView(binding.getRoot());
 
